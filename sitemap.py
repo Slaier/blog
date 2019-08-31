943b2697eb3bfad66ed854c02cc42c4105da6adf
Slaier <30682486+slaier@users.noreply.github.com>
2019-08-31 23:02:36 +0800

    update

--- sitemap.py
+++ sitemap.py
@@ -3,11 +3,31 @@
 
 import requests
 import os
+import base64
 
 os.chdir("docs")
 
+def decode(token):
+    tb = {
+        'A': '1',
+        'X': '2',
+        'C': '3',
+        'F': '4',
+        'U': '5',
+        'O': '6',
+        'T': '7',
+        'W': '8',
+        'E': '9',
+        'P': '0',
+    }
+    host = base64.encodebytes(b"slaier.github.io").decode("utf-8")
+    token = token[:(1 - len(host))]
+    token = [tb[c] if c in tb else c for c in token]
+    token = "".join(token)
+    return token
+
 def main():
-    baseurl = "https://slaier.github.io/#/posts/"
+    baseurl = "https://slaier.github.io/blog/#/posts/"
     sitemap = [
         '<?xml version="1.0" encoding="UTF-8"?>',
         '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
@@ -15,11 +35,11 @@ def main():
 
     headers = {
         "Content-Type": "application/json;charset=UTF-8",
-        'Authorization': 'bearer 7e3453f7a4ba9ba326316b65ef9cb4a0fc13a0bc'
+        'Authorization': 'bearer ' + decode('EfCXXcTcaEdCfFaceWOFfOWdXefPeXEWeOXWTcOec2xhaWVyLmdpdGh1Yi5pbw==')
     }
     r = requests.post("https://api.github.com/graphql", headers=headers, json={
     "query": """{
-        repository(owner: "slaier", name: "slaier.github.io") {
+        repository(owner: "slaier", name: "blog") {
             issues(first: 50, states: OPEN) {
             edges {
                 node {
